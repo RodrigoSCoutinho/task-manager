607ec07f580b6329e28c46bbfd4ec567ebcaf3ab
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-06-26 10:03:46 -0300

    add business rules

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,22 +1,28 @@
-import { Controller, Get, Param, Query } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { randomUUID } from "crypto";
 
 type ParamsUser = {
     id: string;
     idCompany: string;
-}
+};
 
 type QueryUser = {
     p: string;
     r: string;
+};
+
+type BodyUser = {
+    name: string;
+    age: number;
 }
 
-@Controller()
+@Controller("/users")
 
 export class UserController {
 
   //* http://localhost:3000/users/234234234
 
-  @Get("/users/:id/:idCompany")
+  @Get("/:id/:idCompany")
 
   findById(@Param() params:ParamsUser){
     return "Usuario de ID " + params.id + "Empresa ID" + params.idCompany;
@@ -24,8 +30,16 @@ export class UserController {
 
   //? http://localhost:3000/users?p=10&r=100
   
-  @Get("/users/findByPages")
+  @Get("/findByPages")
   findByPages(@Query() queries: QueryUser){
     return "Queries " + queries.p + queries.r;
+  }
+
+  @Post("/create")
+  create(@Body() data: BodyUser){
+    return {
+        ...data,
+        id: randomUUID(),
+    }
   }
 }
