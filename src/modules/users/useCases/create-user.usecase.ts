a3a01380121d8eef487e0b8a320c10f845ab5a6c
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-07-15 20:24:13 -0300

    adding the rules

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -12,6 +12,22 @@ export class CreateUserUseCase {
     constructor(private prisma: PrismaService){}
 
     async execute(data: CreateUserDTO){
+      const user = this.prisma.user.findFirst({
+        where: {
+            OR: [
+                {username: data.username},
+                {email: data.email}
+            ]
+        },
+      });
+
+      if(user){
+        throw new Error("User already exists!");
+      }
+
+      return await this.prisma.user.create({
+         data,
+      })
 
     }
 }
