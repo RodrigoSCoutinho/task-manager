18fac3a9413668db6f3e8acf3870ee154390e850
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-08-08 14:31:57 -0300

    dealing with mistakes

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CreateUserDTO } from "../dto/user.dto";
 
@@ -18,7 +18,7 @@ export class CreateUserUseCase {
       });
 
       if(user){
-        throw new Error("User already exists!");
+        throw new HttpException("User already exists!", HttpStatus.BAD_REQUEST);
       }
 
       return await this.prisma.user.create({
