5bc103d6fc9631d9a8a188d3ce4f51ea49dc8d65
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-08-14 20:39:31 -0300

    adding the hash

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { hash } from 'bcrypt'
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CreateUserDTO } from "../dto/user.dto";
 
@@ -21,8 +22,13 @@ export class CreateUserUseCase {
         throw new HttpException("User already exists!", HttpStatus.BAD_REQUEST);
       }
 
+      //!create a password hashed 
+
+      const passwordHashed = await hash(data.password, 10)
+
       return await this.prisma.user.create({
-         data,
+         ...data,
+         password: passwordHashed,
       })
 
     }
