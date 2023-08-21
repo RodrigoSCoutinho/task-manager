267e17f4c531a2d6a4aa6ea543c00900e498e459
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-08-21 13:38:19 -0300

    fix bugs

--- src/infra/database/prisma.service.ts
+++ src/infra/database/prisma.service.ts
@@ -1,43 +1,43 @@
-import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from '@prisma/client'
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { INestApplication } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-   async onModuleInit() {
-       await this.$connect();
-   }
+  constructor() {
+    super();
+    this.$connect();
+  }
 
-   async enableShutdownHooks(app: INestApplication){
-       this.$on('beforeExit', async () => {
-        await app.close();
-      });
-   };
+  async onModuleInit() {
+    console.log('PrismaService initialized');
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    const shutdown = async () => {
+      await this.$disconnect();
+      await app.close();
+      process.exit(0);
+    };
+
+    process.on('SIGINT', shutdown); // Capturing Ctrl+C
+    process.on('SIGTERM', shutdown); // Capturing termination signal
+  }
 }
-
-
-// import { Injectable, OnModuleInit } from '@nestjs/common';
+// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 // import { PrismaClient } from '@prisma/client';
-// import { INestApplication } from '@nestjs/common';
 
 // @Injectable()
 // export class PrismaService extends PrismaClient implements OnModuleInit {
-//   constructor() {
-//     super();
-//     this.$connect();
-//   }
-
 //   async onModuleInit() {
-//     console.log('PrismaService initialized');
+//     await this.$connect;
 //   }
 
 //   async enableShutdownHooks(app: INestApplication) {
-//     const shutdown = async () => {
-//       await this.$disconnect();
+//     this.$on('beforeExit', async () => {
 //       await app.close();
-//       process.exit(0);
-//     };
-
-//     process.on('SIGINT', shutdown); // Capturing Ctrl+C
-//     process.on('SIGTERM', shutdown); // Capturing termination signal
+//     });
 //   }
 // }
+
+
--- src/modules/users/pipe/create-user.validation.pipe.ts
+++ src/modules/users/pipe/create-user.validation.pipe.ts
@@ -1,21 +1,22 @@
-import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from "@nestjs/common";
-import { CreateUserDTO } from "../dto/user.dto";
-
-@Injectable()
-export class CreateUserValidationPipe implements PipeTransform {
-    transform({name, email, username, password}: CreateUserDTO, metadata: ArgumentMetadata) {
-
-        if(!name || email || password) {
-            throw new HttpException(`[name, email, username, password] is required`,
-            HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-
-        return { 
-            name, 
-            email,
-            username,
-            password
-        }
+import {ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform} from '@nestjs/common';
+import { CreateUserDTO } from '../dto/user.dto';
+  
+  @Injectable()
+  export class CreateUserValidationPipe implements PipeTransform {
+    transform(
+      { name, email, username, password }: CreateUserDTO, metadata: ArgumentMetadata) {
+      if (!name || !email || !username || !password) {
+        throw new HttpException(
+          `[name, email, username, password] is required`,
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+      }
+  
+      return {
+        name,
+        email,
+        username,
+        password,
+      };
     }
-
-}
+  }
