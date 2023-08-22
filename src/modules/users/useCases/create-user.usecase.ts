b6bf5de1c99890eb6640d4ad1f177d4a3697deba
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-08-22 11:59:19 -0300

    implement the interface on CreateUserCase

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -2,30 +2,42 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { hash } from 'bcrypt'
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CreateUserDTO } from "../dto/user.dto";
+import { IUserRepository } from "../repositories/user.repository";
 
 
 @Injectable()
 export class CreateUserUseCase {
-    constructor(private prisma: PrismaService){}
+    constructor(private userRepository: IUserRepository){}
 
     async execute(data: CreateUserDTO){
-      const user = this.prisma.user.findFirst({
-        where: {
-            OR: [{ username: data.username }, { email: data.email }]
-         },
+      const user = await this.userRepository.findByUsernameOrEmail({
+         username: data.username,
+         email: data.email
       });
+
+      // await this.prisma.user.findFirst({
+      //      where: {
+      //          OR: [{ username: data.username }, { email: data.email }]
+      //     },
+      // });
+    
 
       if(user){
         throw new HttpException("User already exists!", HttpStatus.BAD_REQUEST);
       }
 
-      const passwordHashed = await hash(data.password, 10)
+      const password = await hash(data.password, 10);
 
-      return await this.prisma.user.create({
-         data: {
-          ...data,
-          password: passwordHashed,
-         }
-      });
+      return await this.userRepository.save({
+         ...data,
+         password
+      })
+
+      // return await this.prisma.user.create({
+      //    data: {
+      //     ...data,
+      //     password: passwordHashed,
+      //    }
+      // });
     }
 }
